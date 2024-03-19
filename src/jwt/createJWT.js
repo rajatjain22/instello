d92fd57291632e099753c49bf31fa38db1e8c1cd
Rajat Jain <jainr6000@gmail.com>
2024-03-19 23:01:18 +0530

    Latest Update

--- src/jwt/createJWT.js
+++ src/jwt/createJWT.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 const createJWT = (tokenData, expire = "1h") => {
-  console.log(expire)
   const token = jwt.sign(tokenData, process.env.JWT_TOKEN_SECRET, {
     expiresIn: `${expire}`,
   });
